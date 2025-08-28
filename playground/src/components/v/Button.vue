1f38bdb6794a0f4d631ba69f3d9966c9c61266d0
metkm <metinkorkmaz417@gmail.com>
2025-08-28 11:20:57 +0300

    remove comments

--- playground/src/components/v/Button.vue
+++ playground/src/components/v/Button.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
 <template>
   <component
     :is="props.to ? RouterLink : 'button'"
-    class="flex bg-neutral-100 hover:bg-neutral-200 text-sm transition-colors font-medium px-4 py-2 rounded-lg shrink-0"
+    class="flex justify-center bg-neutral-100 hover:bg-neutral-200 text-sm transition-colors font-medium px-4 py-2 rounded-lg shrink-0"
     :to="props.to"
   >
     <slot />
